665eafc18e9ab300c0aa344919af8a23ae23650d
Angel Perez <angel_pdj@hotmail.com>
2023-01-24 15:10:43 -0400

    update

--- Odoo/acciones/search_by_name.py
+++ Odoo/acciones/search_by_name.py
@@ -1,2 +1,10 @@
-sales = model.search([('state', 'in', ['draft','sent']), ('name', '>=', 'PV/2023')])
-raise Warning(sales)
+sales = model.search([('name', 'like', 'PV/2023%')])
+total_sales = []
+total_lines = []
+for s in sales:
+  for ol in s.order_line:
+    if not ol.billing_start_date:
+      total_sales.append(s)
+      total_lines.append(ol)
+raise Warning("El total de ventas es: " + str(len(set(total_sales))) + "\n" + "El total de lineas es: " + str(len(set(total_lines))))
+#Set para no repetir valores
