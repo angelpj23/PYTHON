c156548b6166890242269b0d0e2a87020456ae88
Angel Perez <angel_pdj@hotmail.com>
2023-04-11 17:40:14 -0400

    adding

--- Odoo/acciones/close_activity_by_date.py
+++ Odoo/acciones/close_activity_by_date.py
@@ -1,3 +1,13 @@
+# date of activity
+get_date = "2022/03/15"
+date_to_clear = dateutil.parser.parse(get_date).date()
+
+activities = model.search([])
+for a in activities:
+  if a.create_date.date() <= date_to_clear:
+    a.unlink()
+    
+# date of the register
 activities = model.search([('create_date', '<=', '2022/02/15')])
 for act in activities:
   if act.res_id in crm_active:
